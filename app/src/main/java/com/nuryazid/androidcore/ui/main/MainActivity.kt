e18fb8c7398f24228bcf625bcef48e1aa02f4e13
Zidd <nuvyazid@gmail.com>
2022-03-15 08:46:11 +0700

    update gradle version

--- app/src/main/java/com/nuryazid/androidcore/ui/main/MainActivity.kt
+++ app/src/main/java/com/nuryazid/androidcore/ui/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.nuryazid.androidcore.ui.main
 
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import com.crocodic.core.extension.checkLocationPermission
 import com.nuryazid.androidcore.R
 import com.nuryazid.androidcore.base.BaseActivity
 import com.nuryazid.androidcore.databinding.ActivityMainBinding
@@ -8,5 +11,13 @@ import com.nuryazid.androidcore.databinding.ActivityMainBinding
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        checkLocationPermission {
+            listenLocationChange()
+        }
+    }
+
+    override fun retrieveLocationChange(location: Location) {
+        Log.d("sample", "retreive location at ${location.accuracy}")
     }
 }
