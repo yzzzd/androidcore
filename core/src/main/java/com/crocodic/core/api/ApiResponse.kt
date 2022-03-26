5bcce1a141a80682901ecb114bb7e6355e217757
Zidd <nuvyazid@gmail.com>
2022-03-26 12:58:26 +0700

    add data member and dataAs specific class type

--- core/src/main/java/com/crocodic/core/api/ApiResponse.kt
+++ core/src/main/java/com/crocodic/core/api/ApiResponse.kt
@@ -1,11 +1,8 @@
 package com.crocodic.core.api
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
 import org.json.JSONException
 import org.json.JSONObject
 import retrofit2.HttpException
-import java.net.UnknownHostException
 
 /**
  * Created by @yzzzd on 4/22/18.
@@ -16,11 +13,13 @@ open class ApiResponse(
     var message: String? = null,
     var isTokenExpired: Boolean = false,
     var flagView: Int? = null,
-    var isToast: Boolean = false
+    var isToast: Boolean = false,
+    var data: Any? = null
 ) {
-    fun responseError(error: Throwable? = null, flagView: Int? = null): ApiResponse {
+    fun responseError(error: Throwable? = null, flagView: Int? = null, data: Any? = null): ApiResponse {
         this.status = ApiStatus.ERROR
         this.flagView = flagView
+        this.data = data
 
         if (error is HttpException) {
             error.response()?.errorBody()?.string()?.let { errorBody ->
@@ -39,25 +38,37 @@ open class ApiResponse(
         return this
     }
 
-    fun responseExpired(message: String, flagView: Int? = null): ApiResponse {
+    fun responseExpired(message: String, flagView: Int? = null, data: Any? = null): ApiResponse {
         this.status = ApiStatus.EXPIRED
         this.message = message
         this.flagView = flagView
         this.isTokenExpired = true
+        this.data = data
         return this
     }
 
-    fun responseLoading(message: String? = null, flagView: Int? = null): ApiResponse {
+    fun responseLoading(message: String? = null, flagView: Int? = null, data: Any? = null): ApiResponse {
         this.status = ApiStatus.LOADING
         this.message = message
         this.flagView = flagView
+        this.data = data
         return this
     }
 
-    fun responseSuccess(message: String? = null, flagView: Int? = null): ApiResponse {
+    fun responseSuccess(message: String? = null, flagView: Int? = null, data: Any? = null): ApiResponse {
         this.status = ApiStatus.SUCCESS
         this.message = message
         this.flagView = flagView
+        this.data = data
         return this
+    }
+
+    inline fun <reified T> dataAs(): T? {
+        return try {
+            data as T
+        } catch (e: ClassCastException) {
+            e.printStackTrace()
+            null
+        }
     }
 }
