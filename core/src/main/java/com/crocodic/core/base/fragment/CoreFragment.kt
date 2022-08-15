106cd315da3c993b28247ce4e751d18b44b18af9
Zidd <nuvyazid@gmail.com>
2022-08-15 14:52:40 +0700

    rollback nullable binding in fragment

--- core/src/main/java/com/crocodic/core/base/fragment/CoreFragment.kt
+++ core/src/main/java/com/crocodic/core/base/fragment/CoreFragment.kt
@@ -11,29 +11,23 @@ import androidx.fragment.app.Fragment
 import com.crocodic.core.helper.ImagePreviewHelper
 import com.crocodic.core.helper.util.ClickPrevention
 
-abstract class CoreFragment<VB : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
-    Fragment(), ClickPrevention {
+abstract class CoreFragment<VB : ViewDataBinding>(@LayoutRes private val layoutRes: Int) : Fragment(), ClickPrevention {
 
-    private var _binding: VB? = null
-    protected val binding get() = _binding!!
+    protected var binding: VB? = null
 
     open var title: String = ""
     open var hasLoadedOnce = false
 
     val imagePreview by lazy { context?.let { ImagePreviewHelper(it) } }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
-        binding.lifecycleOwner = this
-        return binding.root
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding?.lifecycleOwner = this
+        return binding?.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 }
