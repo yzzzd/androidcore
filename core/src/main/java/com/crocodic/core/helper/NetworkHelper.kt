6bde8cda30b8a7577caffaea38e9e0f0fa7ff257
Zidd <nuvyazid@gmail.com>
2023-06-08 11:23:33 +0700

    add helper to provide okhttp and api service

--- core/src/main/java/com/crocodic/core/helper/NetworkHelper.kt
+++ core/src/main/java/com/crocodic/core/helper/NetworkHelper.kt
@@ -4,12 +4,20 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
+import com.crocodic.core.helper.okhttp.SSLTrust
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
 
 /**
  * Created by @yzzzd on 4/22/18.
@@ -35,13 +43,15 @@ object NetworkHelper {
         }
     }
 
-    fun hasActiveInternet(context: Context, connected: (result: Boolean) -> Unit) {
+    fun hasActiveInternet(context: Context, testUrl: String = "https://www.google.com/generate_204", connected: (result: Boolean) -> Unit) {
         //https://www.google.com/generate_204
         //https://clients3.google.com/generate_204
         CoroutineScope(Dispatchers.IO).launch {
             if (isNetworkAvailable(context)) {
                 try {
-                    val urlc = URL("https://www.google.com/generate_204").openConnection().apply {
+                    val urlc = withContext(Dispatchers.IO) {
+                        URL(testUrl).openConnection()
+                    }.apply {
                         setRequestProperty("User-Agent", "Test")
                         setRequestProperty("Connection", "close")
                         connectTimeout = 1500
@@ -56,4 +66,41 @@ object NetworkHelper {
             }
         }
     }
+
+    fun provideOkHttpClient(timeOut: Long = 90, withUnsafeTrustManager: Boolean = true, withInterceptor: Boolean = false): OkHttpClient {
+
+        val okHttpClient = OkHttpClient().newBuilder()
+            .connectTimeout(timeOut, TimeUnit.SECONDS)
+            .readTimeout(timeOut, TimeUnit.SECONDS)
+            .writeTimeout(timeOut, TimeUnit.SECONDS)
+
+        if (withUnsafeTrustManager) {
+            val unsafeTrustManager = SSLTrust().createUnsafeTrustManager()
+            val sslContext = SSLContext.getInstance("SSL").apply {
+                init(null, arrayOf(unsafeTrustManager), null)
+            }
+            okHttpClient.sslSocketFactory(sslContext.socketFactory,  unsafeTrustManager)
+        }
+
+        if (withInterceptor) {
+            val interceptors = HttpLoggingInterceptor()
+            interceptors.level = HttpLoggingInterceptor.Level.BODY
+            okHttpClient.addInterceptor(interceptors)
+        }
+
+        return okHttpClient.build()
+    }
+
+    inline fun <reified T> provideApiService(baseUrl: String, okHttpClient: OkHttpClient = provideOkHttpClient(), converterFactory: List<Converter.Factory>? = null): T {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+
+        converterFactory?.forEach {
+            retrofit.addConverterFactory(it)
+        }
+
+        return retrofit.build().create(T::class.java)
+    }
+
 }
